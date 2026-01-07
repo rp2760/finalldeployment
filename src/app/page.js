c074fd83e002bbffc1f33ef5711189c4d445f618
rp2760 <rp9510810648@gmail.com>
2026-01-07 14:28:28 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>hello world 
+      how are you 
+      that is my first project for deploy 
+    </h1>
+    </>
+  );
+}
